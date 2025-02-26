7f89ed6d3c4a49114099c1319bd6d03b57220935
Jasmine <91042728+certainforest@users.noreply.github.com>
2025-02-26 00:42:53 -0600

    check: working on templating function

--- src/instruct.py
+++ src/instruct.py
@@ -2,22 +2,30 @@ import pandas as pd
 import torch
 import time
 from typing import List, Dict, Any
+import yaml 
+import os
 
-def apply_instruct_format(inputs: list, model: str = None, append_response_start = True, is_math = False) -> List:
+config_path = os.path.join(os.path.dirname(__file__), '..', "config", "config.yaml")
+config_path = os.path.abspath(config_path)
+
+with open(config_path, "r") as f:
+    config = yaml.safe_load(f)
+
+def apply_instruct_format(inputs: list, model = None, append_response_start = True, is_math = False) -> List:
     """
     Modify prompts to incorporate DeepSeek's usage recommendations: 
         1. Ask model to initiate response with "<think>\n"
         2. If math, include "please reason step by step, and put your final answer within \boxed{}"
         see: https://huggingface.co/deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B
     """
-    df_transformed = df.copy()
+    formatted_inputs = []
 
     
 
-    if is_math:
-        formatted += "please reason step by step, and put your final answer within \boxed{}"
-
-    if append_response_start: 
-        formatted += "\n<|assistant|>"
+        if is_math:
+            formatted += "please reason step by step, and put your final answer within \boxed{}"
+    
+        if append_response_start: 
+            formatted += "\n<|assistant|>"
 
     return formatted
